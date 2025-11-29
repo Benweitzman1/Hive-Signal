5b99ad3476e0a1d7b9edbc1a070a943655ced5da
Benweitzman1 <Benweitzman1@gmail.com>
2025-11-29 14:10:32 +0200

    Installed Devise for API mode with username authentication

--- backend/app/models/message.rb
+++ backend/app/models/message.rb
@@ -3,12 +3,13 @@ class Message
   include Mongoid::Timestamps
 
   field :content, type: String
-  field :session_id, type: String
   field :phone_number, type: String
 
-  validates :content, presence: true
-  validates :session_id, presence: true
-  validates :phone_number, presence: true
+  belongs_to :user
 
-  scope :by_session, ->(session_id) { where(session_id: session_id).desc(:created_at) }
+  validates :content, presence: true
+  validates :phone_number, presence: true
+  validates :user_id, presence: true
+
+  scope :by_user, ->(user_id) { where(user_id: user_id).desc(:created_at) }
 end
